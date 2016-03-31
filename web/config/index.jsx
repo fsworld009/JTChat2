16fffa6ffc4aee6642a82f5dd6372ea4acedf125
WorldFS <fsworld009@gmail.com>
2016-03-30 22:34:59 -0400

    default configs file, change jsx indent size to 2

--- web/config/index.jsx
+++ web/config/index.jsx
@@ -12,87 +12,87 @@ import { Router, Route, IndexRoute, Link, browserHistory } from 'react-router';
 var rootPath = "/config/";
 
 var MainMenu = React.createClass({
-    render: function(){
-        var menuItems = [
-            {path: "site", label: "Site"},
-            {path: "user", label: "User"},
-            {path: "theme", label: "Theme"},
-            {path: "profile", label: "Profile"}
-        ];
-        var currentPath = (this.props.currentPath || "").replace(/^\/config\/([^\/]*)(.*$)/, function(match, $1){
-            return $1;
-        });
-        var currentMenuItem = _.find(menuItems, function(menuItem){
-            return menuItem.path == currentPath;
-        });
-        if(currentMenuItem){
-            currentMenuItem.selected = true;
-        }else{
-            menuItems[0].selected = true;
-        }
-        return (
-            <div className="ui blue secondary pointing menu">
-                {menuItems.map(function(menuItem){
-                    return <Link key={menuItem.path} className={"item"+ (menuItem.selected? " active":"")} to={rootPath + menuItem.path}>{menuItem.label}</Link>;
-                })}
-            </div>
-        );
+  render: function(){
+    var menuItems = [
+      {path: "site", label: "Site"},
+      {path: "user", label: "User"},
+      {path: "theme", label: "Theme"},
+      {path: "profile", label: "Profile"}
+    ];
+    var currentPath = (this.props.currentPath || "").replace(/^\/config\/([^\/]*)(.*$)/, function(match, $1){
+      return $1;
+    });
+    var currentMenuItem = _.find(menuItems, function(menuItem){
+      return menuItem.path == currentPath;
+    });
+    if(currentMenuItem){
+      currentMenuItem.selected = true;
+    }else{
+      menuItems[0].selected = true;
     }
+    return (
+      <div className="ui blue secondary pointing menu">
+        {menuItems.map(function(menuItem){
+          return <Link key={menuItem.path} className={"item"+ (menuItem.selected? " active":"")} to={rootPath + menuItem.path}>{menuItem.label}</Link>;
+        })}
+      </div>
+    );
+  }
 });
 
 var Site = React.createClass({
-    render: function(){
-        return (<div>Site</div>);
-    }
+  render: function(){
+    return (<div>Site</div>);
+  }
 });
 
 var User = React.createClass({
-    render: function(){
-        return (<div>User</div>);
-    }
+  render: function(){
+    return (<div>User</div>);
+  }
 });
 
 var Theme = React.createClass({
-    render: function(){
-        return (<div>Theme</div>);
-    }
+  render: function(){
+    return (<div>Theme</div>);
+  }
 });
 
 var Profile = React.createClass({
-    render: function(){
-        return (<div>Profile</div>);
-    }
+  render: function(){
+    return (<div>Profile</div>);
+  }
 });
 
 var NoMatch = React.createClass({
-    render: function(){
-        return (<div>NoMatch</div>);
-    }
+  render: function(){
+    return (<div>NoMatch</div>);
+  }
 });
 
 var App = React.createClass({
-    render: function(){
-        return (
-            <div>
-                <h1 className="ui header">JTChat2 Configuration</h1>
-                <MainMenu currentPath={this.props.location.pathname}/>
-                {this.props.children}
-            </div>
-        );
-    }
+  render: function(){
+    return (
+      <div>
+        <h1 className="ui header">JTChat2 Configuration</h1>
+        <MainMenu currentPath={this.props.location.pathname}/>
+        {this.props.children}
+      </div>
+    );
+  }
 
 });
 
 ReactDOM.render(
-    <Router history={browserHistory}>
-      <Route path={rootPath} component={App}>
-        <Route path="site" component={Site}/>
-        <Route path="user" component={User}/>
-        <Route path="theme" component={Theme}/>
-        <Route path="profile" component={Profile}/>
-        <Route path="*" component={Site}/>
-        <IndexRoute component={Site} />
-      </Route>
-    </Router>,
+  <Router history={browserHistory}>
+    <Route path={rootPath} component={App}>
+    <Route path="site" component={Site}/>
+    <Route path="user" component={User}/>
+    <Route path="theme" component={Theme}/>
+    <Route path="profile" component={Profile}/>
+    <Route path="*" component={Site}/>
+    <IndexRoute component={Site} />
+    </Route>
+  </Router>,
   document.getElementById('app')
 );
