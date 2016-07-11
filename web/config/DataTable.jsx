ae1585dccbe7251d1215fe813c62bae86f2a995f
WorldFS <fsworld009@gmail.com>
2016-07-10 21:03:23 -0400

    User view mode

--- web/config/DataTable.jsx
+++ web/config/DataTable.jsx
@@ -1,4 +1,5 @@
 var $ = require("jquery");
+var _ = require("lodash");
 var util = require("./util.js");
 require("../css/dataTables.font-awesome.css");
 require("../js/jquery.dataTables");
@@ -6,24 +7,37 @@ require("../js/dataTables.semanticui.js");
 
 
 var DataTable = React.createClass({
+  propTypes: {
+    "options": React.PropTypes.object.isRequired
+  },
+
   componentDidMount: function(){
     var $this = util.getJqueryDom(this);
-    $this.dataTable({
-      data:[{col1:"col1",col2:"col2"},{col1:"col1-2",col2:"col2-2"}],
-      columns: [{title: "Col 1", data: "col1"}, {title: "Col 2", data: "col2"}],
-      destroy: true,
-      lengthChange: false
-    });
+    if(this.props.options.columns instanceof Array && this.props.options.columns.length>0 && this.props.options.data instanceof Array){
+      _.each(this.props.options.columns, function(column){
+        if(typeof column.defaultContent == 'undefined'){
+          column.defaultContent = "-";
+        }
+      });
+      $this.dataTable(this.props.options);
+      this.hasDataTableInstance = true;
+    }else{
+      this.hasDataTableInstance = false;
+    }
   },
 
   componentDidUpdate: function(){
-    var $this = util.getJqueryDom(this);
-    $this.DataTable().draw();
+    if(this.hasDataTableInstance){
+      var $this = util.getJqueryDom(this);
+      $this.DataTable().draw();
+    }
   },
 
   componentWillUnmount: function(){
-    var $this = util.getJqueryDom(this);
-    $this.DataTable().destroy();
+    if(this.hasDataTableInstance){
+      var $this = util.getJqueryDom(this);
+      $this.DataTable().destroy();
+    }
   },
   render: function(){
     var className = util.mergeClassName(this, ["ui celled table", this.props.className]);
--- web/config/User.jsx
+++ web/config/User.jsx
@@ -1,9 +1,15 @@
 var React = require("react");
+var _ = require("lodash");
 var DataTable = require("./DataTable.jsx");
 import { connect } from 'react-redux';
+import { getId } from './ajax.js';
 
 function mapStateToProps(state){
-  return {sites: state};
+  return {
+    usersById: state.get("usersById"),
+    users: state.get("users"),
+    sitesById: state.get("sitesById")
+  };
 }
 
 function mapDispatchToProps(dispatch){
@@ -13,10 +19,31 @@ function mapDispatchToProps(dispatch){
 
 
 var User = React.createClass({
+  getSiteFromSiteId: function(siteId){
+    var siteRefId = getId(siteId);
+    var site = this.props.sitesById.get(siteRefId);
+    return site.get("displayName");
+  },
+
   render: function(){
-    console.log(this.props);
+    console.log("User",this.props);
+    var users = this.props.usersById.toJS();
+    users = _.values(users);
+
+    var options = {
+      data: users,
+      columns: [
+        {title: "Site", data: "siteId", render: this.getSiteFromSiteId},
+        {title: "URL ID", data: "urlId"},
+        {title: "Username", data: "username"},
+        {title: "Display Name", data: "displayName"}
+      ],
+      lengthChange: false,
+      paging: false,
+      order: []
+    };
     return (
-      <DataTable></DataTable>
+      <DataTable options={options}></DataTable>
     );
   }
 });
