f8678bcf379d0033e39f4cc7b8079e83a1c3c98b
WorldFS <fsworld009@gmail.com>
2016-07-10 22:35:15 -0400

    render dataTable cells by myself in order to render React components

--- web/config/DataTable.jsx
+++ web/config/DataTable.jsx
@@ -8,43 +8,84 @@ require("../js/dataTables.semanticui.js");
 
 var DataTable = React.createClass({
   propTypes: {
-    "options": React.PropTypes.object.isRequired
+    "options": React.PropTypes.object
   },
 
   componentDidMount: function(){
     var $this = util.getJqueryDom(this);
-    if(this.props.options.columns instanceof Array && this.props.options.columns.length>0 && this.props.options.data instanceof Array){
-      _.each(this.props.options.columns, function(column){
-        if(typeof column.defaultContent == 'undefined'){
-          column.defaultContent = "-";
-        }
-      });
-      $this.dataTable(this.props.options);
-      this.hasDataTableInstance = true;
-    }else{
-      this.hasDataTableInstance = false;
-    }
+console.log("options", this.props.options);
+    $this.dataTable(this.props.options);
   },
 
   componentDidUpdate: function(){
-    if(this.hasDataTableInstance){
-      var $this = util.getJqueryDom(this);
-      $this.DataTable().draw();
-    }
+    var $this = util.getJqueryDom(this);
+    $this.DataTable().draw();
   },
 
   componentWillUnmount: function(){
-    if(this.hasDataTableInstance){
-      var $this = util.getJqueryDom(this);
-      $this.DataTable().destroy();
-    }
+    var $this = util.getJqueryDom(this);
+    $this.DataTable().destroy();
   },
+
+  renderDataCell: function(data, columns){
+    var cells = [];
+    _.each(columns, function(column){
+      var defaultContent = typeof column.defaultContent == "undefined"? "-" : column.defaultContent;
+      var cellData;
+      var attributes = ["data", "render"];
+      _.each(attributes, function(attribute){
+        switch(typeof column[attribute]){
+          default: case "string":
+            cellData = data[column[attribute]];
+            break;
+          case "function" :
+            var passInData = attribute=="data"? data: cellData;
+            cellData = column[attribute](passInData, column);
+            break;
+          case "undefined" :
+            break;
+        }
+      });
+      cellData = (typeof cellData == "undefined" || cellData == null) ? defaultContent : cellData;
+      cells.push(cellData);
+    });
+    var key=0;
+    return (cells.map(function(cell){key++;return (<td key={key}>{cell}</td>);}));
+  },
+
+  renderRow: function(data, columns){
+    var rows = [];
+    _.each(data, function(dataObject){
+      rows.push(this.renderDataCell(dataObject, columns));
+    }.bind(this)); 
+    var key=0;
+    return (rows.map(function(row){key++;return (<tr key={key}>{row}</tr>);}));
+  },
+
   render: function(){
     var className = util.mergeClassName(this, ["ui celled table", this.props.className]);
+    var options = this.props.options;
+    this.isValidTable = options.columns instanceof Array && options.columns.length>0 && options.data;
+    if(this.isValidTable){
+        this.thead = (<thead><tr>
+          {options.columns.map(function(column){
+            return (<th key={column.title}>{column.title}</th>);
+          })} 
+        </tr></thead>);
+        
+
+      this.tbody = (<tbody>
+        {this.renderRow(options.data, options.columns)}
+      </tbody>);
+      delete options.data;
+      delete options.columns;
+    }
+
+
     return (
       <table className={className}>
-        <tbody></tbody>
-        <thead></thead>
+        {this.thead}
+        {this.tbody}
       </table>
     );
   }
--- web/config/User.jsx
+++ web/config/User.jsx
@@ -1,8 +1,10 @@
 var React = require("react");
+var ReactServer = require("react-dom/server");
 var _ = require("lodash");
 var DataTable = require("./DataTable.jsx");
 import { connect } from 'react-redux';
 import { getId } from './ajax.js';
+import {IconButton} from './Semantic.jsx';
 
 function mapStateToProps(state){
   return {
@@ -19,6 +21,16 @@ function mapDispatchToProps(dispatch){
 
 
 var User = React.createClass({
+  renderActionButtons: function(userId){
+    userId = getId(userId);
+    return (
+      <span>
+        <IconButton iconClass="fa fa-pencil" className="green" popup-content="Edit" route={"/config/user/edit/" + userId}></IconButton>
+        <IconButton iconClass="fa fa-trash" className="red" popup-content="Delete" route={"/config/user/delete/" + userId}></IconButton>
+      </span>
+    );
+  },
+
   getSiteFromSiteId: function(siteId){
     var siteRefId = getId(siteId);
     var site = this.props.sitesById.get(siteRefId);
@@ -26,7 +38,6 @@ var User = React.createClass({
   },
 
   render: function(){
-    console.log("User",this.props);
     var users = this.props.usersById.toJS();
     users = _.values(users);
 
@@ -36,7 +47,11 @@ var User = React.createClass({
         {title: "Site", data: "siteId", render: this.getSiteFromSiteId},
         {title: "URL ID", data: "urlId"},
         {title: "Username", data: "username"},
-        {title: "Display Name", data: "displayName"}
+        {title: "Display Name", data: "displayName"},
+        {title: " ", data: "id", render: this.renderActionButtons}
+      ],
+      columnDefs: [
+        { targets: [4], orderable: false, width: "8em"}
       ],
       lengthChange: false,
       paging: false,
