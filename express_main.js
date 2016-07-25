8664708b80ba9dccfe2622b112e7030f1e1ca840
WorldFS <fsworld009@gmail.com>
2016-07-24 21:20:43 -0400

    auto reload express server

--- express_main.js
+++ express_main.js
@@ -14,10 +14,16 @@ var theme_reader = require(path.resolve(basePath, "./core/theme_reader.js"));
 var environment = process.env.NODE_ENV;
 
 app.use(bodyParser.json());
+//app.set('views', './views')
+app.set('view engine', 'ejs');
 
 app.get("/config/*", function(req, res, next){
     req.url = "/config/";
     next();
+});
+
+app.get("/config/", function(req, res, next){
+    res.render("config",{env: environment});
 });
 
 app.get("/profiles/", function(req, res, next){
@@ -49,37 +55,50 @@ app.put("/profiles/", function(req, res){
         res.send({success: true});
     });
 });
-app.use('/', express.static(path.resolve(__dirname + '/web')));
+app.use('/js/', express.static(path.resolve(__dirname + '/web/js/')));
+app.use('/css/', express.static(path.resolve(__dirname + '/web/css/')));
+app.use('/fonts/', express.static(path.resolve(__dirname + '/web/fonts/')));
 
-var webpack = require('webpack');
-//var WebpackDevServer = require('webpack-dev-server');
-var config = require('./webpack.dev-server.config.js');
-var compiler = webpack(config);
+if(environment === "production"){
+    //production
+    app.listen(3000, function () {
+        console.log('Example app listening on port 3000!');
+    });
+}else{
+    //use express.static in production
+    var webpack = require('webpack');
+    //var WebpackDevServer = require('webpack-dev-server');
+    var config = require('./webpack.dev-server.config.js');
+    var compiler = webpack(config);
 
-var webpackDevMiddleware = require("webpack-dev-middleware");
-app.use("/bundle/",webpackDevMiddleware(compiler, {
-    // options
-}));
-app.use(require("webpack-hot-middleware")(compiler));
+    var webpackDevMiddleware = require("webpack-dev-middleware");
+    app.use("/bundle/",webpackDevMiddleware(compiler, {
+        // options
+    }));
+    app.use(require("webpack-hot-middleware")(compiler));
 
-// new WebpackDevServer(webpack(config), {
-//    hot: true,
-//    historyApiFallback: true,
-//    proxy: {
-//      "*": "http://localhost:3000"
-//    }
-// }).listen(3001, 'localhost', function (err, result) {
-//    if (err) {
-//      console.log(err);
-//    }
-//
-//    console.log('webpack-dev-server Listening on port 3001');
-// });
-
-
+    // new WebpackDevServer(webpack(config), {
+    //    hot: true,
+    //    historyApiFallback: true,
+    //    proxy: {
+    //      "*": "http://localhost:3000"
+    //    }
+    // }).listen(3001, 'localhost', function (err, result) {
+    //    if (err) {
+    //      console.log(err);
+    //    }
+    //
+    //    console.log('webpack-dev-server Listening on port 3001');
+    // });
 
 
+    var reload = require('reload');
+    var http = require('http');
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+    var server = http.createServer(app);
+    reload(server, app);
+    server.listen(3000, function(){
+        console.log('JTChat 2 (Dev) listening on port 3000');
+    });
+}
+
