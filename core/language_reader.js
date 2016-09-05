753c2ac48478427d17a1c187fa110103f457cf29
WorldFS <fsworld009@gmail.com>
2016-09-04 20:11:42 -0400

    use English as fallback when other lang files are missing properties

--- core/language_reader.js
+++ core/language_reader.js
@@ -11,6 +11,7 @@ Promise.promisifyAll(fs);
 
 
 var database;
+var defaultLangFileNameRegExp = new RegExp("^en");
 
 function parseJson(jsonString, defaultObject){
     //inject comment handle in the future
@@ -38,22 +39,35 @@ function readLanguageFiles(){
     database.set("languages", []).value();
     return fs.readdirAsync(languageBasePath).then(function(languageFiles){
         var langJsonFileList = [];
+        var enLangJsonFile;
         
         _.each(languageFiles, function(filename){
             //console.log(filename);
-            langJsonFileList.push( path.resolve(languageBasePath, filename ));
+            var readPath = path.resolve(languageBasePath, filename );
+            if(filename.search(defaultLangFileNameRegExp) > -1){
+                enLangJsonFile = readPath;
+            }else{
+                langJsonFileList.push( readPath);
+            }
         });
         //console.log(langJsonFileList);
+        //read English first
+        return fs.readFileAsync(enLangJsonFile, "utf-8").then(function(jsonString){
+            var enLangJson = parseJson(jsonString);
+            database.get("languages").push(enLangJson).value();
 
-        return Promise.all (_.map(langJsonFileList, function(jsonPath){
+            //then read other languages
+            return Promise.all (_.map(langJsonFileList, function(jsonPath){
                 return fs.readFileAsync(jsonPath,"utf-8").then(function(jsonString){
                     //console.log(jsonString);
                     json = parseJson(jsonString);
-                    database.get("languages").push(json).value();
+                    var thisLangJson = _.merge({}, enLangJson, json);
+                    database.get("languages").push(thisLangJson).value();
                 });
-            })
-        ).then(function(){
+            }));
+        }).then(function(){
              database.set("languageByCode", database.get("languages").keyBy("langCode").value()).value();
+             return Promise.resolve();
         });
 
     });
@@ -64,7 +78,7 @@ function refresh(){
     database = lowdb();
     readLanguageFiles().then(function(){
         console.log("after readLanguageFiles()");
-        console.log(database.get("languages").keyBy("langCode").value());
+        console.log(database.get("languages").value());
         console.log(database.get("languageByCode").value());
     });
 }
