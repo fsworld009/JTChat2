ff80d5c1eb429407056bdb92d7f3490bef8091f2
WorldFS <fsworld009@gmail.com>
2016-09-11 21:23:04 -0400

    User list page

--- web/config/User.jsx
+++ web/config/User.jsx
@@ -3,14 +3,13 @@ var ReactServer = require("react-dom/server");
 var _ = require("lodash");
 var DataTable = require("./DataTable.jsx");
 import { connect } from 'react-redux';
-import { getId } from './ajax.js';
 import {Segment, IconButton} from './Semantic.jsx';
+import {lang, getDB} from "./database.js";
 
 function mapStateToProps(state){
   return {
     usersById: state.get("usersById"),
-    users: state.get("users"),
-    sitesById: state.get("sitesById")
+    users: state.get("users")
   };
 }
 
@@ -22,43 +21,48 @@ function mapDispatchToProps(dispatch){
 
 var User = React.createClass({
   renderActionButtons: function(userId){
-    userId = getId(userId);
     return (
       <span>
-        <IconButton iconClass="fa fa-pencil" className="green" popup-content="Edit" route={"/config/user/edit/" + userId}></IconButton>
-        <IconButton iconClass="fa fa-trash" className="red" popup-content="Delete" route={"/config/user/delete/" + userId}></IconButton>
+        <IconButton iconClass="fa fa-pencil" className="green" popup-content={lang("common.edit")} route={"/config/user/edit/" + userId}></IconButton>
+        <IconButton iconClass="fa fa-trash" className="red" popup-content={lang("common.delete")} route={"/config/user/delete/" + userId}></IconButton>
       </span>
     );
   },
 
   getSiteFromSiteId: function(siteId){
-    var siteRefId = getId(siteId);
-    var site = this.props.sitesById.get(siteRefId);
-    return site.get("displayName");
+    return lang("site.sitesById."+siteId+".name");
   },
 
   render: function(){
-    var users = this.props.usersById.toJS();
-    users = _.values(users);
+    var users = this.props.users.toJS() || [];
+    var usersById = this.props.usersById.toJS() || {};
+    var language = lang("user");
+    users = users.map(function(userId) {
+      return usersById[userId] || {};
+    });
+    console.log("users", users);
 
     var options = {
       data: users,
+      info: false,
       columns: [
-        {title: "Site", data: "siteId", render: this.getSiteFromSiteId},
-        {title: "Username", data: "username"},
-        {title: "Display Name", data: "displayName"},
-        {title: "URL ID", data: "urlId"},
+        {title: lang("site.title"), data: "siteId", render: this.getSiteFromSiteId},
+        {title: lang("user.username"), data: "username"},
+        {title: lang("user.displayName"), data: "displayName"},
         {title: " ", data: "id", render: this.renderActionButtons}
       ],
       columnDefs: [
-        { targets: [4], orderable: false, width: "8em"}
+        { targets: [3], orderable: false, width: "8em"}
       ],
       lengthChange: false,
       paging: false,
-      order: []
+      order: [],
+      language: {
+        search: lang("common.search")
+      }
     };
     return (
-      <Segment style={{position:"relative"}} title="User">
+      <Segment style={{position:"relative"}} title={language.title}>
         <DataTable className="selectable" options={options}></DataTable>
         <div style={{position: "absolute", left: "1em", top: "1em"}}><IconButton iconClass="fa fa-plus" className="green" popup-content="New" route={"/config/user/new/"}></IconButton></div>
       </Segment>
