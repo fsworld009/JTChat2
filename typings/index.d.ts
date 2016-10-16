96e7e979d53401c245e5a06bfb8718f7764db8da
WorldFS <fsworld009@gmail.com>
2016-10-16 17:30:52 -0400

    express.js typings definition

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,4 +1,5 @@
 /// <reference path="globals/bluebird/index.d.ts" />
+/// <reference path="globals/express/index.d.ts" />
 /// <reference path="globals/github-electron/index.d.ts" />
 /// <reference path="globals/jquery/index.d.ts" />
 /// <reference path="globals/lodash/index.d.ts" />
